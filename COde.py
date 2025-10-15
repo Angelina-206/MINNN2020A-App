568a5f33c73882104b1fb5eb9daddecaf72890e1
2691164 <2691164@students.wits.ac.za>
2025-10-15 23:47:45 +0200

    app configuration

--- COde.py
+++ COde.py
@@ -1,1 +1,12 @@
+from flask import Flask, request, redirect, url_for, render_template_string, session
+from werkzeug.security import generate_password_hash, check_password_hash
+import pandas as pd
+import os, csv
+import plotly.express as px
+import folium
+from datetime import timedelta
+from functools import wraps
 
+app = Flask(__name__)
+app.secret_key = "Group7"
+app.permanent_session_lifetime = timedelta(hours=2)
